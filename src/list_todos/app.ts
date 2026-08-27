59de193898b8deef9a3833e54a117d76236799cb
Van Ngo <siunhan33@gmail.com>
2026-08-27 01:57:05 -0500

    Update list_todo api with db connection

--- src/list_todos/app.ts
+++ src/list_todos/app.ts
@@ -1,6 +1,32 @@
-export const lambda_handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "list_todos hit" })
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DEFAULT_USER_ID } from "../shared/constant";
+
+const client = new DynamoDB({ region: process.env.REGION });
+const docClient = DynamoDBDocument.from(client);
+
+async function queryItems(userId: string) {
+  var params = {
+    TableName: process.env.TABLE,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: { ":userId": userId }
   };
+  const data = await docClient.query(params);
+  return data.Items;
+}
+
+export const lambda_handler = async () => {
+  try {
+    const items = await queryItems(DEFAULT_USER_ID)
+    return { 
+      statusCode: 200,
+      body: JSON.stringify(items) 
+    };
+  } catch (error) {
+    console.error("[list_todos/app.ts] Failed with: " + error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to fetch todos."})
+    };
+  }
 };
