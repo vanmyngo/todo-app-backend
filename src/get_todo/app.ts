22913fd61d1922c9677a754504a60003e739773d
Van Ngo <siunhan33@gmail.com>
2026-08-31 20:27:40 -0500

    Update get_todo

---
+++ src/get_todo/app.ts
@@ -0,0 +1,53 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { DEFAULT_USER_ID } from "shared";
+
+const client = new DynamoDB({ region: process.env.REGION });
+const docClient = DynamoDBDocument.from(client);
+
+async function getTodo(userId: string, taskId: string) {
+  const params = {
+    TableName: process.env.TABLE,
+    Key: { 
+      userId,
+      taskId,
+    },
+  };
+
+  const result = await docClient.get(params);
+  return result;
+};
+
+export const lambda_handler = async (event: APIGatewayProxyEvent) => {
+  const taskId = event.pathParameters?.taskId;
+
+  if (!taskId || typeof taskId !== "string" || taskId.trim() === "") {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Task is required." }),
+    };
+  }
+
+  try {
+    const result = await getTodo(DEFAULT_USER_ID, taskId);
+
+    if (!result.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Todo not found." }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+  } catch (error) {
+    console.error("[get_todo/app.ts] " + error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to get todo." }),
+    };
+  }
+};
--- src/get_todos/app.ts
+++
@@ -1,7 +0,0 @@
-export const lambda_handler = async (event) => {
-  const id = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "get_todos hit", id })
-  };
-};
